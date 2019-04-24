38951b0d25372d24030be992415df9d918f9526b
git-trading <kba.asm.12@gmail.com>
2019-04-24 14:26:32 +0800

    added babel and file loader for images.

--- webpack.config.js
+++ webpack.config.js
@@ -8,15 +8,36 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [{
-      test: /\.html$/,
-      use: [ {
-        loader: 'html-loader',
-        options: {
-          minimize: true
+    rules: [
+      {
+        test: /\.html$/,
+        use: [ {
+          loader: 'html-loader',
+          options: {
+            minimize: true
+          }
+        }]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
         }
-      }],
-    }]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ]
   },
   plugins: [
     new HtmlWebPackPlugin({
