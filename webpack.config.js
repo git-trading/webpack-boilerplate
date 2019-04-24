1f4511bb51de7c802064798704fc594f7a5a362e
git-trading <kba.asm.12@gmail.com>
2019-04-24 13:30:59 +0800

    Initial commit for webpack-test with html-webpack-plugin.

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
